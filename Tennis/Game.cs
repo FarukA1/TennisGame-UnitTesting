048bb4b800e387a53e4c793bce9cde7966c7c704
farukawofeso <farukawofeso@outlook.com>
2022-11-30 21:05:32 +0000

    * Game.cs: * Tennis.csproj:
    * Test.cs: new commit

--- Tennis/Game.cs
+++ Tennis/Game.cs
@@ -3,8 +3,116 @@ namespace Tennis
 {
     public class Game
     {
-        public Game()
-        {
+        private string player1Point { get; set; } = "0";
+        private string player2Point { get; set; } = "0";
+
+        public Game(){}
+
+        public string returnPlayer1Score(){
+            return player1Point;
+        
         }
+
+        public string returnPlayer2Score(){
+            return player2Point;
+        
+        }
+
+        public void addPointToPlayer1()
+        {
+            if (player1Point == "0"){
+                player1Point = "15";
+            }
+            else if (player1Point == "15"){
+                player1Point = "30";
+            }
+            else if (player1Point == "30"){
+                player1Point = "40";
+            }
+            else if (player1Point == "40" && player2Point == "0"){
+                player1Point = "W";
+           }
+            else if (player1Point == "40" && player2Point == "15"){
+                player1Point = "W";
+           }
+            else if (player1Point == "40" && player2Point == "30"){
+                player1Point = "W";
+           }
+            else if (player1Point == "40" && player2Point == "40"){
+                player1Point = "A";
+               //player2Point = "40"
+           }
+            else if (player1Point == "40" && player2Point == "A"){
+                player1Point = "40";
+                player2Point = "40";
+           }
+            else if (player1Point == "A" || player2Point == "40"){
+                player1Point = "W";
+           }
+
+        }
+
+        public void  addPointToPlayer2()
+        {
+
+            if (player2Point == "0"){
+                player2Point = "15";
+            }
+            else if (player2Point == "15"){
+                player2Point = "30";
+            }
+            else if (player2Point == "30"){
+                player2Point = "40";
+            }
+            else if (player2Point == "40" && player1Point == "0"){
+                player2Point = "W";
+           }
+            else if (player2Point == "40" && player1Point == "15"){
+                player2Point = "W";
+           }
+            else if (player2Point == "40" && player1Point == "30"){
+                player2Point = "W";
+           }
+            else if (player2Point == "40" && player1Point == "40"){
+                player2Point = "A";
+               //player1Point = "40"
+           }
+            else if (player2Point == "40" && player1Point == "A"){
+                player1Point = "40";
+                player2Point = "40";
+           }
+            else if (player2Point == "A" || player1Point == "40"){
+                player1Point = "W";
+           }
+        }
+
+        public bool player1Won()
+        {
+        
+            if (returnPlayer1Score() == "W"){
+                return true;
+        }
+            return false;
+        
+        }
+
+        public bool player2Won()
+        {
+        
+        if (returnPlayer2Score() == "W"){
+                return true;
+        }
+            return false;
+        
+        }
+
+        public bool completeGame()
+        {
+
+        if (player1Won() == true || player2Won() == true){
+                return true;
+        }
+            return false;
+    }
     }
 }
--- Tennis/Test.cs
+++ Tennis/Test.cs
@@ -5,9 +5,149 @@ namespace Tennis
     [TestFixture()]
     public class Test
     {
-        [Test()]
-        public void TestCase()
+        Game game;
+
+        [SetUp]
+        public void SetUp()
         {
+            game = new Game();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            game = null;
+        }
+
+        public void reachDeuce()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                game.addPointToPlayer1();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                game.addPointToPlayer2();
+            }
+        }
+
+        [Test()]
+        public void testZeroPoints()
+        {
+            Assert.AreEqual(game.returnPlayer1Score(), "0", "P1 score correct with 0 points");
+            Assert.AreEqual(game.returnPlayer2Score(), "0", "P2 score correct with 0 points");
+        }
+
+        [Test()]
+        public void testScoreOnePoint()
+        {
+            game.addPointToPlayer1();
+            Assert.AreEqual(game.returnPlayer1Score(), "15", "P1 score correct with 1 points");
+
+            game.addPointToPlayer2();
+            Assert.AreEqual(game.returnPlayer2Score(), "15", "P2 score correct with 1 points");
+        }
+
+        [Test()]
+        public void testScoreTwoPoint()
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                game.addPointToPlayer1();
+            }
+            Assert.AreEqual(game.returnPlayer1Score(), "30", "P1 score correct with 2 points");
+
+            for (int i = 0; i < 2; i++)
+            {
+                game.addPointToPlayer2();
+            };
+            Assert.AreEqual(game.returnPlayer2Score(), "30", "P2 score correct with 2 points");
+        }
+
+        [Test()]
+        public void testScoreThreePoint()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                game.addPointToPlayer1();
+            }
+            Assert.AreEqual(game.returnPlayer1Score(), "40", "P1 score correct with 3 points");
+
+            for (int i = 0; i < 3; i++)
+            {
+                game.addPointToPlayer2();
+            };
+            Assert.AreEqual(game.returnPlayer2Score(), "40", "P2 score correct with 3 points");
+        }
+
+        [Test()]
+        public void testSimpleWinP1()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                game.addPointToPlayer1();
+            }
+            Assert.IsTrue(game.player1Won(), "P1 win after 4 consecutive points");
+        }
+
+        [Test()]
+        public void testSimpleWinP2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                game.addPointToPlayer2();
+            }
+            Assert.IsTrue(game.player2Won(), "P2 win after 4 consecutive points");
+        }
+
+        [Test()]
+        public void testReachingDeuce()
+        {
+            reachDeuce();
+            Assert.AreEqual(game.returnPlayer1Score(), "40", "P1 score correct reaching Deuce");
+            Assert.AreEqual(game.returnPlayer2Score(), "40", "P2 score correct reaching Deuce");
+            Assert.IsFalse(game.player1Won());
+            Assert.IsFalse(game.player2Won());
+            Assert.IsFalse(game.completeGame());
+        }
+
+        [Test()]
+        public void testAdvantageP1()
+        {
+            reachDeuce();
+            game.addPointToPlayer1();
+            Assert.AreEqual(game.returnPlayer1Score(), "A", "P1 score correct with P1 Advantage");
+            Assert.AreEqual(game.returnPlayer2Score(), "40", "P2 score correct with P1 Advantage");
+        }
+
+        [Test()]
+        public void testAdvantageP2()
+        {
+            reachDeuce();
+            game.addPointToPlayer2();
+            Assert.AreEqual(game.returnPlayer2Score(), "A", "P2 score correct with P1 Advantage");
+            Assert.AreEqual(game.returnPlayer1Score(), "40", "P1 score correct with P1 Advantage");
+        }
+
+        [Test()]
+        public void testGameWinP1()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                game.addPointToPlayer1();
+            }
+            Assert.IsTrue(game.completeGame());
+        }
+
+        [Test()]
+        public void testGameWinP2()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                game.addPointToPlayer2();
+            }
+            Assert.IsTrue(game.completeGame());
         }
     }
 }
